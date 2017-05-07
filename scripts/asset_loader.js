c25a4ec30e09a6b4715f141922a202a235fd48bb
jakehilborn <jakehilborn@gmail.com>
2017-05-07 08:59:06 -0700

    Support for loading arbitrary video/audio URLs

---
+++ scripts/asset_loader.js
@@ -0,0 +1,53 @@
+/*jslint browser:true */
+
+const v_mp4 = "video/mp4";
+const a_mp3 = "audio/mp3";
+
+function loadURL(sourceElement, targetElement) {
+    const isVideo = targetElement.includes("video");
+
+    const url = document.getElementById(sourceElement).value;
+    const type = inferMime(url, isVideo);
+
+    let media;
+    if (isVideo) {
+        media = document.createElement("video");
+        media.width = "320";
+        media.height = "240";
+    } else { //audio
+        media = document.createElement("audio");
+    }
+    media.src = url;
+    media.type = type;
+    media.controls = true;
+
+    const toReplace = document.getElementById(targetElement);
+    toReplace.parentNode.replaceChild(media, toReplace);
+    media.id = targetElement; //Reuse targetElement id after replacing targetElement
+}
+
+function inferMime(url, isVideo) {
+    if (url.includes("://youtube.com") || url.includes("www.youtube.com") || url.startsWith("youtube.com")) {
+        //youtube
+    } else if (url.includes(".mp4")) {
+        return v_mp4;
+    } else if (url.includes(".webm")) {
+
+    } else if (url.includes(".ogv")) {
+
+    } else if (url.includes(".mp3")) {
+        return a_mp3;
+    } else if (url.includes(".ogg")) {
+
+    } else if (url.includes("mp4")) {
+        return v_mp4;
+    } else if (url.includes("webm")) {
+
+    } else if (url.includes("ogv")) {
+
+    } else if (url.includes("mp3")) {
+        return a_mp3;
+    } else if (url.includes("ogg")) {
+
+    }
+}
